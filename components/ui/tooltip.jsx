70cda3712fb988118ebefcfc528122afb33c4f38
Fr3zyy <123799898+Fr3zyy@users.noreply.github.com>
2024-11-14 01:33:44 +0300

    Updated tooltip component.

--- components/ui/tooltip.jsx
+++ components/ui/tooltip.jsx
@@ -19,11 +19,7 @@ const TooltipContent = React.forwardRef(({ className, sideOffset = 4, ...props }
       "z-50 overflow-hidden rounded-md font-semibold bg-white text-black px-3 py-1.5 text-sm shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
     )}
-    {...props}
-  >
-    <TooltipPrimitive.Arrow className="fill-white h-2 w-4" />
-    {props.children}
-  </TooltipPrimitive.Content>
+    {...props} />
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
